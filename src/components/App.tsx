526d54a195554dbf5f5210f3d001ed83b78b4870
fkaspare <fabian.kasparek@stud.htwk-leipzig.de>
2022-09-02 21:57:50 +0200

    Style landing page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,11 +4,11 @@ import "./../stylesheets/App";
 export const App = () => {
   return (
     <div id="app">
-      <p id="app-headline">Wandelndes Wörterbuch</p>
-
-      <p id="app-description">Finde Wörter mit dem Anfangsbuchstaben!</p>
-
-      <button id="app-start-button">Start</button>
+      <div id="app-body">
+        <p id="app-headline">Wandelndes Wörterbuch</p>
+        <p id="app-description">Finde Wörter mit dem Anfangsbuchstaben!</p>
+        <button id="app-start-button">Start</button>
+      </div>
     </div>
   );
 };
